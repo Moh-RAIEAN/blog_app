8f859fb958263cd22f84cfc58f37d58cd2232348
Hasib Al Raiean <hasibalraiean@gmail.com>
2024-12-24 16:53:42 +0600

    [edited] -- added icons and edited root api

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -6,7 +6,16 @@ const serverApiRoutes: Router = Router();
 serverApiRoutes.get(
   '/',
   catchAsync(async (_, res) => {
-    res.json({ ok: true });
+    res.json({
+      appName: 'Blog App',
+      description:
+        'The Blog App is a backend for a blogging platform where users can write, update, and delete their blogs. The system will have two roles: Admin and User. The Admin has special permissions to manage users and their blogs, while users can perform CRUD operations on their own blogs. The backend will include secure authentication, role-based access control, and a public API for viewing blogs with search, sort, and filter functionalities.',
+      author: {
+        name: 'Muh.Hasib Al-Raiean',
+        email: 'hasibalraiean@gmail.com',
+        gitHub: 'https://github.com/Moh-RAIEAN',
+      },
+    });
   }),
 );
 
