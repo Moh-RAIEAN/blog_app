91de327998ace3755849d46ee17e761c11456d76
Hasib Al Raiean <hasibalraiean@gmail.com>
2024-12-24 17:16:17 +0600

    [updated] -- updated build directory

--- dist/app/routes/index.js
+++ dist/app/routes/index.js
@@ -17,7 +17,15 @@ const catchAsync_1 = __importDefault(require("../utils/catchAsync"));
 const apiRoutes_1 = __importDefault(require("./apiRoutes"));
 const serverApiRoutes = (0, express_1.Router)();
 serverApiRoutes.get('/', (0, catchAsync_1.default)((_, res) => __awaiter(void 0, void 0, void 0, function* () {
-    res.json({ ok: true });
+    res.json({
+        appName: 'Blog App',
+        description: 'The Blog App is a backend for a blogging platform where users can write, update, and delete their blogs. The system will have two roles: Admin and User. The Admin has special permissions to manage users and their blogs, while users can perform CRUD operations on their own blogs. The backend will include secure authentication, role-based access control, and a public API for viewing blogs with search, sort, and filter functionalities.',
+        author: {
+            name: 'Muh.Hasib Al-Raiean',
+            email: 'hasibalraiean@gmail.com',
+            gitHub: 'https://github.com/Moh-RAIEAN',
+        },
+    });
 })));
 apiRoutes_1.default.forEach((route) => serverApiRoutes.use(`/api${route.path}`, route.route));
 exports.default = serverApiRoutes;
